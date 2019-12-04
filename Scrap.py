f2f2f98edb5f2553b88dd90aa8104239f390fd75
Megha Patel <meghapatel1903@gmail.com>
2019-12-04 15:25:21 -0500

    Final Call

--- Scrap.py
+++ Scrap.py
@@ -1,48 +1,43 @@
 import requests
 import json
 
-# # Variables
-Itemsinpage = ""
-for i in (1,10):
-    PageNumber = i
-print(PageNumber)
-# # Headers and Parameters
-parameters = {
-    "itemsInPage":Itemsinpage,
-    "Page":PageNumber,
-    "machineName":"Server"
-}
-Headers = {
-    "X-API-KEY" : "2e6ab90f97734051bfc4b6a45823215a",
-    "Accept": "application/json"
-}
-# # getting response from the Atera Url
-response = requests.get("https://app.atera.com/api/v3/agents", headers=Headers, params=parameters)
-print ("Status Code:{}".format(response.status_code))
 
-# # Function to convert Scrapped data to Proper Json format
+def Get_All_Agents():
+    All_Agents = []
+    # # Variables
+    Itemsinpage = "50"
+    for i in range(1,10):
+        PageNumber = i
+        # # Headers and Parameters
+        parameters = {
+            "itemsInPage":Itemsinpage,
+            "Page":PageNumber,
+            "machineName":"Server"
+        }
+        Headers = {
+            "X-API-KEY" : "2e6ab90f97734051bfc4b6a45823215a",
+            "Accept": "application/json"
+        }
+        # # getting response from the Atera Url
+        response = requests.get("https://app.atera.com/api/v3/agents", headers=Headers, params=parameters)
+        # print ("Status Code:{}".format(response.status_code))
+        All_Agents.append(response.json()["items"])
+    return All_Agents
+
+        # print(len(Agents_All))
+
+def Get_Offline_Servers():        
+    Offline_Servers = []
+    for i in Get_All_Agents():
+        for agent in i:
+            if agent["Monitored"] == True and agent["Online"] == False:
+                a = agent["CustomerName"] + agent["AgentName"] + "  IS OFFLINE"
+                Offline_Servers.append(a) 
+    return Offline_Servers
+
+    # Function to convert Scrapped data to Proper Json format
 def jprint(obj):
     text = json.dumps(obj, sort_keys=True , indent=4)
     return text
 
-# # Printing Clean JSON Data or the Requested Page
-# print(jprint(response.json()))
-
-Agents_All = response.json()["items"]
-
-print(len(Agents_All))
-for agent in Agents_All:
-    if agent["Monitored"] == True:
-        print (agent["CustomerName"], agent["AgentName"])
-
-
-# print(len(Agents_All))
-
-# def Get_Server():
-#     # Servers = {}
-#     for agent in Agents_All:
-#         if agent['SystemName'] == "SERVER":
-#             print(agent["CustomerName"], agent["MachineName"])
-
-# Get_Server()
-# # print(Servers)
+print (len(Get_Offline_Servers()))
--- send_sms.py
+++ send_sms.py
@@ -1,3 +1,4 @@
+from Scrap import Get_All_Agents, Get_Offline_Servers,jprint
 import os
 from twilio.rest import Client
 
@@ -6,10 +7,11 @@ auth_token = "2de34a34c3e12272895d2b923661b426"
 
 client = Client(account_sid, auth_token)
 
-message = client.messages.create(
-        to = "+16476878190",
-        from_ = "+17059990096",
-        body = "Hello How are you!"
-)
-
-print(message.sid)
+a = (Get_Offline_Servers())
+for i in a:
+    print(i)
+    message = client.messages.create(
+            to = "+17057702540",
+            from_ = "+17059990096",
+            body = i
+        )
